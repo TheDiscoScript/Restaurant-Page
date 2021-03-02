7a0aefe4ca67a820c4f7a452702775be6e930570
Willdooo <wollmada@gmail.com>
2021-03-02 19:08:25 +0100

    Made helper function to create seperate divs

--- src/menu.js
+++ src/menu.js
@@ -1,23 +1,38 @@
-//helper function
+//helper function (arg, arg2)
+// creates div (need class?)
+//classes for display flex
+//->
+// name of item (arg)(h3), prize (arg2)(p), phote (img)
+// how to src img tho? `${arg}`
+function helpItems(name, prize) {
+  const divItem = document.createElement("div");
+  divItem.classList.add("menuDivItem");
+
+  const itemName = document.createElement("h3");
+  itemName.textContent = name;
+
+  const itemPrize = document.createElement("p");
+  itemPrize.textContent = prize + ",- Kč";
+
+  const itemPhoto = document.createElement("img");
+  itemPhoto.src = `images/${name}.png`;
+
+  divItem.appendChild(itemName);
+  divItem.appendChild(itemPhoto);
+  divItem.appendChild(itemPrize);
+
+  return divItem;
+}
 
 function createMenuDiv() {
   const menuDiv = document.createElement("div");
   menuDiv.id = "midContentMenu";
 
-  const item0 = document.createElement("div");
-  const item1 = document.createElement("div");
-  const item2 = document.createElement("div");
-  const item3 = document.createElement("div");
+  menuDiv.appendChild(helpItems("Ramen", "159"));
+  menuDiv.appendChild(helpItems("Quesadilla", "159"));
+  menuDiv.appendChild(helpItems("Massaman", "159"));
+  menuDiv.appendChild(helpItems("Limonade", "65"));
 
-  item0.textContent = "0";
-  item1.textContent = "1";
-  item2.textContent = "2";
-  item3.textContent = "3";
-
-  menuDiv.appendChild(item0);
-  menuDiv.appendChild(item1);
-  menuDiv.appendChild(item2);
-  menuDiv.appendChild(item3);
   return menuDiv;
 }
 function renderMenu() {
